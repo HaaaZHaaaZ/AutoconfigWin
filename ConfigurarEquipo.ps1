20290c06cdc9e6906720152b5c4fe75a2a7b1838
HaaaZHaaaZ <brayan95andres@hotmail.com>
2025-03-04 22:28:09 +0100

    Archivo de configuracion Anydesk Open cmd

--- ConfigurarEquipo.ps1
+++ ConfigurarEquipo.ps1
@@ -216,12 +216,8 @@ function Configurar-AnyDesk {
         echo $password | & "C:\Program Files (x86)\AnyDeskMSI\AnyDeskMSI.exe" --set-password
         Write-Host "Contraseña de AnyDesk configurada correctamente." -ForegroundColor Green
         
-        ##configurar el ID de AnyDesk
-        # Obtener el ID de AnyDesk
-        $anydeskID = cmd.exe /c 'for /f "delims=" %i in ('"C:\Program Files (x86)\AnyDeskMSI\AnyDeskMSI.exe" --get-id') do @echo %i'
-        $anydeskID = $anydeskID -replace "`r`n", ""
-        Write-Host "ID de AnyDesk: $anydeskID" -ForegroundColor Cyan
-        Write-Host "Contraseña de AnyDesk: Soporte2025" -ForegroundColor Cyan
+        # Obtener el ID de AnyDesk en una nueva ventana de cmd
+        Start-Process cmd.exe -ArgumentList "/c @echo off && for /f ""delims="" %i in ('""C:\Program Files (x86)\AnyDeskMSI\AnyDeskMSI.exe"" --get-id') do set CID=%i && echo AnyDesk ID is: %CID% && pause"
     }
     catch {
         Write-Host "Error al configurar la contraseña de AnyDesk: $_" -ForegroundColor Red
